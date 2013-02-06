e4d7ee013580a395dce11596d095a965cbd027e4
me-programmers <me.programmers@gmail.com>
2013-02-06 20:27:19 +0800

    first stage

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AppBpusulsel::Application.config.secret_token = '6d5124447410fab10f3377911aa22aa1bc7052c1beedeb583936feac5d5306951318d01deab284a6a6fc53751dd39575fac8c707c6fa7c1e0b6db07890abacd0'
